2f53b578c5b21af32e6d5ab07ffc783d312675e9
NinjaHunterC-LOL <hc1960@g.rit.edu>
2026-09-22 08:26:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function newGame() {
     phase: "aiming", // aiming | in flight | celebrating
     currentPlayer: 1,
     round: 1,
-    windSpeed: generateWindSpeed(),
+    windSpeed: generateWindSpeed(100),
     bomb: {
       x: undefined,
       y: undefined,
